dd71e2ea56e91e0f2cacead89e6c9f1b65ff858a
damiansra <damiansra@gmail.com>
2013-04-05 14:54:54 +0300

    erased old files

--- README.md
+++
@@ -1,4 +0,0 @@
-em-petsc
-========
-
-PETSc and Fortran solver to the full wave, 1D and 2D,  Maxwell's electrodynamics equations for time varying, non-linear materials, using PETSc
--- create_da.py
+++
@@ -1,6 +0,0 @@
-from petsc4py import PETSc
-da = PETSc.DA().create([16,16], stencil_width=0)
-
-#da.view()
-print da.comm.getRank(), da.getGhostCorners()
-#print da.getGhostCorners()
