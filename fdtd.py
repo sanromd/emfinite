85ebc4d0222a5d1e535c44e793579c8b7f4be2a3
damiansra <damiansra@gmail.com>
2013-06-23 23:45:28 +0300

    some general changes to use an interface similar to that observed in em-pyclaw_2d

--- fdtd.py
+++ fdtd.py
@@ -7,87 +7,99 @@ petsc4py.init(sys.argv)
 
 import numpy as np
 
-# space
-xx = 1e-6
-yy = 500e-6
-
-ddx = 1#5e-9
-ddy = 1#5e-9
-
-nx = 35
-ny = 35
-
 # -------- GLOBAL SCALAR DEFINITIONS -----------------------------
+
 # ======== all definitions are in m,s,g unit system.
 n_frames = 30
 # ....... dimensions .............................................
-x_lower = 0.0e-6
-x_upper = 100e-6                    # lenght [m]
-y_lower = 0.0e-6
-y_upper = 1.0e-6                    # notice that for multilayer this is value will be over-written
+x_lower = 0.0
+x_upper = 100.0e-6                    # lenght [m]
+y_lower = 0.0
+y_upper = 10.0e-6                   # notice that for multilayer this is value will be over-written
 # ........ material properties ...................................
 
 # vacuum
 eo = 8.854187817e-12            # vacuum permittivity   - [F/m]
 mo = 4e-7*np.pi                 # vacuum peremeability  - [V.s/A.m]
-co = 1/np.sqrt(eo*mo)           # vacuum speed of light - [m/s]
-zo = np.sqrt(eo/mo)
+co = 1.0/np.sqrt(eo*mo)           # vacuum speed of light - [m/s]
+zo = np.sqrt(mo/eo)
 # material
-mat_shape = 'vacuum'           # material definition: homogeneous, interface, rip (moving perturbation), multilayered
-num_aux = 3
-# background refractive index
-bkg_er = 1.5
-bkg_mr = 1.5
-bkg_n  = np.sqrt(bkg_er*bkg_mr)
-bkg_e  = eo*bkg_er
-bkg_m  = mo*bkg_mr
+mat_shape = 'homogeneous'           # material definition: homogeneous, interface, rip (moving perturbation), multilayered
+# parameters needed for pml calculation
+num_pml = 8
+norder = 3
+Ro = 1.0e-6
+
+# initialize background refractive index and etas
+eta      = np.ones([3])
+bkg_n    = np.ones([2])
+
+bkg_n[0] = np.sqrt(eta[0]*eta[2])
+bkg_n[1] = np.sqrt(eta[1]*eta[2])
+
 
 # if interface declare position
-x_change = x_upper/2
+x_change = (x_upper-x_lower)/2.0
+y_change = (y_upper-y_lower)/2.0
 
-# set moving refractive index parameters
-rip_vx_e    = 0.0*co    # replace here the value of x
-rip_vx_m    = rip_vx_e
-rip_vy_e    = 0.0*co
-rip_vy_m    = rip_vy_e
+# set moving refractive index or gaussian2D parameters
+rip_velocity = np.zeros([2,3])
+rip_offset   = np.zeros([2,3])
+rip_sigma    = np.zeros([2,3])
+delta_eta    = np.zeros([3])
 
-rip_xoff_e  = 10e-6
-rip_xoff_m  = rip_xoff_e
-rip_yoff_e  = rip_xoff_e
-rip_yoff_m  = rip_xoff_e
+rip_offset[0,:].fill((x_upper-x_lower)/2.0)
+rip_offset[1,:].fill((y_upper-y_lower)/2.0)
+rip_sigma[0,:].fill((x_upper-x_lower)/25.0)
+rip_sigma[1,:].fill((y_upper-y_lower)/25.0)
+rip_sigma.fill(10e-6)
+rip_sigma.fill(10e-6)
+rip_sigma2 = rip_sigma**2
 
-rip_xsig_e  = 10.0e-6
-rip_xsig_m  = rip_xsig_e
-rip_ysig_e  = .9*y_upper/2
-rip_ysig_m  = rip_ysig_e
-s_x_e       = rip_xsig_e**2
-s_x_m       = rip_xsig_m**2
-s_y_e       = rip_ysig_e**2
-s_y_m       = rip_ysig_m**2
-
-prip        = 0.1
-deltan      = prip*(bkg_n) # assumes epsilon = mu
-d_e         = deltan #*(2.0*1.5+deltan)
-d_m         = deltan #*(2.0*1.5+deltan)
+delta_eta = np.zeros([3])
+delta_eta = 0.1*eta
 
 # set multilayer parameters
-
-# multilayered definition
 n_layers = 2
-layers = np.zeros([n_layers,7]) # _layer:  eps mu N t chi2e chi2m chi3e chi3m
+layers = np.zeros([n_layers,9]) # _layer:  eps mu N t chi2e chi2m chi3e chi3m
 layers[0,0] = 1.5
 layers[0,1] = 1.5
-layers[0,2] = 10
-layers[0,3] = 15e-9
+layers[0,2] = 1.5
+layers[0,3] = 10
+layers[0,4] = 15e-9
 layers[1,0] = 2.5
 layers[1,1] = 2.5
-layers[1,2] = layers[0,2] - 1
-layers[1,3] = 50e-9
+layers[1,2] = 2.5
+layers[1,3] = layers[0,3] - 1
+layers[1,4] = 50e-9
 N_layers = 5
 if mat_shape=='multilayer':
-    y_upper = N_layers*np.sum(layers[:,3])+layers[0,3]
-    tlp = np.sum(layers[:,3])
+    y_upper = N_layers*np.sum(layers[:,4])+layers[0,4]
+    tlp = np.sum(layers[:,4])
     mlp = np.floor(tlp/1e-9)
+
+# set non-linear parameters of the material
+chi2 = chi3 = np.zeros( [3], order='F')
+
+# ........ excitation - initial conditoons .......................
+
+# pre-allocate arrays
+ex_sigma     = np.ones([3])    # x,y,t
+ex_offset    = np.zeros([3])
+ex_amplitude = np.ones([3])
+ex_kvector   = np.zeros([2])
+
+# fill arrays and set respective values
+ex_type   = 'plane'
+ex_lambda = 1e-6
+ex_sigma[0:1] = 1.0*ex_lambda
+ex_sigma[2]   = (y_upper-y_lower)/2.0
+ex_offset[2]  = (y_upper-y_lower)/2.0
+
+# post calculations
+omega    = 2.0*np.pi*co/ex_lambda   # frequency
+k        = 2.0*np.pi/ex_lambda      # k vector magnitude
+ex_kvector[0] = k                   # propagation along the x-direction
 
 # ........ Boundary settings settings .................
 
@@ -101,52 +113,29 @@ aux_bc_x_upper = 'pml'
 aux_bc_y_upper = 'metallic'
 aux_bc_y_upper = 'metallic'
 
-# parameters needed for pml calculation
-num_pml = 8
-norder = 3
-Ro = 1.0e-6
-
-# ........ excitation - initial conditoons .......................
-
-ex_type  = 'plane'
-alambda  = 1e-6             # wavelength
-ex_t_sig = 1.0*alambda          # width in time (pulse only)
-ex_x_sig = 1.0*alambda          # width in the x-direction (pulse)
-ex_y_sig = y_upper-y_lower
-ex_toff  = 0.0                  # offset in time
-ex_xoff  = 0.0                  # offset in the x-direction
-ex_yoff  = y_upper/2            # offset in the y-direction
-omega    = 2.0*np.pi*co/alambda # frequency
-k        = 2.0*np.pi/alambda
-amp_Ex   = 0.
-amp_Ey   = 1.
-amp_Hz   = 1.
 
 
 # ........ pre-calculations for wave propagation .................
 
-v_r = 1./bkg_n
-v = co*v_r
-ex_vx = v
-ex_vy = 0.0
-ex_kx = k
-ex_ky = 0.0
+v = co/bkg_n.min()
 
 # Grid - mesh settings
-mx = np.floor(40*(x_upper-x_lower)/alambda)
+nx = np.floor(10*(x_upper-x_lower)/ex_lambda)
 if mat_shape=='multilayer':
-    my = np.floor((y_upper-y_lower)/1e-9)
+    ny = np.floor((y_upper-y_lower)/1e-9)
 else:
-    my = np.floor(20*(y_upper-y_lower)/alambda)
+    ny = np.floor(10*(y_upper-y_lower)/ex_lambda)
 
 ddx = (x_upper-x_lower)/mx
 ddy = (y_upper-y_lower)/my
-ddt = dt = 1#0.90/(co*np.sqrt(1.0/(ddx**2)+1.0/(ddy**2)))
-max_steps = 250000
+ddt = dt=0.50/(co*np.sqrt(1.0/(ddx**2)+1.0/(ddy**2)))
+max_steps = 1000000
 t_final = (x_upper-x_lower)/v
+print t_final
 
 dxdt = 1
 dydt = 1
+
 
 # -------- GLOBAL FUNCTION DEFINITIONS --------------
 
@@ -184,56 +173,54 @@ def etar(da,ddx,ddy,t=0):
     X = np.linspace(xi,xf,xf-xi)*ddx
     Y = np.linspace(yi,yf,yf-yi)*ddy
     y,x = np.meshgrid(Y,X)
-    eta = np.empty( [3,len(X),len(Y)], order='F')
+    eta_out = np.empty( [3,len(X),len(Y)], order='F')
 
     if mat_shape=='gaussian1dx':
-        u_x_e = x - rip_vx_e*t - rip_xoff_e
-        u_x_m = x - rip_vx_m*t - rip_xoff_m
-        u_y_e = y - rip_vy_e*t - rip_yoff_e
-        u_y_m = y - rip_vy_m*t - rip_yoff_m
+        u_x_eta1 = x - rip_velocity[0,0]*t - rip_offset[0,0]
+        u_x_eta2 = x - rip_velocity[0,1]*t - rip_offset[0,1]
+        u_x_eta3 = x - rip_velocity[0,2]*t - rip_offset[0,2]
+        u_y_eta1 = y - rip_velocity[1,0]*t - rip_offset[1,0]
+        u_y_eta2 = y - rip_velocity[1,1]*t - rip_offset[1,1]
+        u_y_eta3 = y - rip_velocity[1,2]*t - rip_offset[1,2]
 
-        u_e = (u_x_e/rip_xsig_e)**2 + (u_y_e/rip_ysig_e)**2
-        u_m = (u_x_m/rip_xsig_m)**2 + (u_y_m/rip_ysig_m)**2
+        u_eta1 = (u_x_eta1/rip_sigma[0,0])**2 + (u_y_eta1/rip_sigma[1,0])**2
+        u_eta2 = (u_x_eta2/rip_sigma[0,1])**2 + (u_y_eta2/rip_sigma[1,1])**2
+        u_eta3 = (u_x_eta3/rip_sigma[0,2])**2 + (u_y_eta3/rip_sigma[1,2])**2
 
-        eta[0,:,:] = d_e*np.exp(-u_e) + bkg_er
-        eta[1,:,:] = d_e*np.exp(-u_e) + bkg_er
-        eta[2,:,:] = d_m*np.exp(-u_m) + bkg_mr
+        eta_out[0,:,:] = delta_eta[0]*np.exp(-u_eta1) + eta[0]
+        eta_out[1,:,:] = delta_eta[1]*np.exp(-u_eta2) + eta[1]
+        eta_out[2,:,:] = delta_eta[2]*np.exp(-u_eta3) + eta[2]
     elif mat_shape=='homogeneous':
-        eta[0,:,:] = bkg_er
-        eta[1,:,:] = bkg_er
-        eta[2,:,:] = bkg_mr
-    elif mat_shape=='vacuum':
-        eta[0,:,:] = 1.0
-        eta[1,:,:] = 1.0
-        eta[2,:,:] = 1.0
+        eta_out[0,:,:] = eta[0]
+        eta_out[1,:,:] = eta[1]
+        eta_out[2,:,:] = eta[2]
     elif mat_shape=='interfacex':
-        eta[0,:,:] = 1*(x<x_change) + 4*(x>=x_change)
-        eta[1,:,:] = 1*(x<x_change) + 4*(x>=x_change)
-        eta[2,:,:] = 1*(x<x_change) + 4*(x>=x_change)
+        eta_out[0,:,:] = 1*(x<x_change) + 4*(x>=x_change)
+        eta_out[1,:,:] = 1*(x<x_change) + 4*(x>=x_change)
+        eta_out[2,:,:] = 1*(x<x_change) + 4*(x>=x_change)
     elif mat_shape=='interfacey':
-        yy = y_upper-y_lower
-        eta[0,:,:] = 1*(y<yy/2) + 4*(x>=yy/2)
-        eta[1,:,:] = 1*(y<yy/2) + 4*(x>=yy/2)
-        eta[2,:,:] = 1*(y<yy/2) + 4*(x>=yy/2)
+        eta_out[0,:,:] = 1*(y<y_change/2) + 4*(x>=y_change/2)
+        eta_out[1,:,:] = 1*(y<y_change/2) + 4*(x>=y_change/2)
+        eta_out[2,:,:] = 1*(y<y_change/2) + 4*(x>=y_change/2)
     elif mat_shape=='multilayer':
         for n in range(0,N_layers):
             yi = n*tlp
             for m in range(0,n_layers):
                 if m==0:
-                    eta[0,:,:] = layers[m,0]*(yi<y)*(y<=yi+layers[m,3])
-                    eta[1,:,:] = layers[m,1]*(yi<y)*(y<=yi+layers[m,3])
-                    eta[2,:,:] = layers[m,1]*(yi<y)*(y<=yi+layers[m,3])
+                    eta_out[0,:,:] = layers[m,0]*(yi<y)*(y<=yi+layers[m,3])
+                    eta_out[1,:,:] = layers[m,1]*(yi<y)*(y<=yi+layers[m,3])
+                    eta_out[2,:,:] = layers[m,2]*(yi<y)*(y<=yi+layers[m,3])
                 else:
-                    eta[0,:,:] = layers[m,0]*(yi+layers[m-1,3]<y)*(y<=yi+layers[m,3])
-                    eta[1,:,:] = layers[m,1]*(yi+layers[m-1,3]<y)*(y<=yi+layers[m,3])
-                    eta[2,:,:] = layers[m,1]*(yi+layers[m-1,3]<y)*(y<=yi+layers[m,3])
+                    eta_out[0,:,:] = layers[m,0]*(yi+layers[m-1,3]<y)*(y<=yi+layers[m,3])
+                    eta_out[1,:,:] = layers[m,1]*(yi+layers[m-1,3]<y)*(y<=yi+layers[m,3])
+                    eta_out[2,:,:] = layers[m,2]*(yi+layers[m-1,3]<y)*(y<=yi+layers[m,3])
 
 
-        eta[0,:,:] = layers[0,0]*(N_layers*tlp<y)*(y<=N_layers*tlp+layers[0,3])
-        eta[1,:,:] = layers[0,1]*(N_layers*tlp<y)*(y<=N_layers*tlp+layers[0,3])
-        eta[2,:,:] = layers[0,1]*(N_layers*tlp<y)*(y<=N_layers*tlp+layers[0,3])
+        eta_out[0,:,:] = layers[0,0]*(N_layers*tlp<y)*(y<=N_layers*tlp+layers[0,4])
+        eta_out[1,:,:] = layers[0,1]*(N_layers*tlp<y)*(y<=N_layers*tlp+layers[0,4])
+        eta_out[2,:,:] = layers[0,2]*(N_layers*tlp<y)*(y<=N_layers*tlp+layers[0,4]) 
 
-    return eta
+    return eta_out
 
 def qinit(Q1,Q2,Q3,da):
     """
@@ -241,12 +228,26 @@ def qinit(Q1,Q2,Q3,da):
     """
     nx, ny = da.getSizes()
     (xi, xf), (yi, yf) = da.getRanges()
+
     q1 = da.getVecArray(Q1)
     q2 = da.getVecArray(Q2)
     q3 = da.getVecArray(Q3)
-    q1[:,:] = 0.0
-    q2[:,:] = 0.0
-    q3[:,:] = 0.0
+
+    if ex_type=='off':
+        X = np.linspace(xi,xf,xf-xi)*ddx
+        Y = np.linspace(yi,yf,yf-yi)*ddy
+        y,x = np.meshgrid(Y,X)
+        dd1 = x_upper-x_lower
+        dd2 = y_upper-y_lower
+        sdd = ex_lambda
+        r2 = (x-dd1/2.0)**2 #+ (y-dd2/2.0)**2
+        q1[:,:] = 0.0
+        q2[:,:] = zo*np.exp(-r2/(sdd**2))
+        q3[:,:] = 1.0*np.exp(-r2/(sdd**2))
+    else:
+        q1[:,:] = 0.0
+        q2[:,:] = 0.0
+        q3[:,:] = 0.0
 
 def qbc(Q1,Q2,Q3,da):
     """
@@ -309,13 +310,44 @@ def bc_scattering(Q1,Q2,Q3,da,axis,side):
     """
     nx, ny = da.getSizes()
     (xi, xf), (yi, yf) = da.getRanges()
-
+    X = np.linspace(xi,xf,xf-xi)*ddx
+    Y = np.linspace(yi,yf,yf-yi)*ddy
+    y,x = np.meshgrid(Y,X)
+    pulseshape = np.zeros( [len(X),len(Y)], order='F')
+    harmonic = np.zeros( [len(X),len(Y)], order='F')
+    if ex_type=='plane':
+        pulseshape = 1.0
+        harmonic = np.sin(omega*t)
+    elif ex_type=='gauss-beam':
+        pulseshape = np.exp(-(y - ex_offset[2])**2/ex_sigma[2]**2)
+        harmonic = np.sin(omega*t)
+    elif ex_type=='gauss_pulse':
+        pulseshape = np.exp(-(ex_vx*(t-ex_offset[3]))**2/ex_sigma[3]**2 - (y - ex_offset[2] - ex_vy*(t-ex_offset[3]))**2/ex_sigma[2]**2)
+        harmonic = np.sin(omega*t)
+    elif ex_type=='plane_pulse':
+        pulseshape = np.exp(-(ex_vx*(t-ex_offset[3]))**2/ex_sigma[3]**2)
+        harmonic = np.sin(omega*t)
+    elif ex_type=='simple_pulse2D':
+        pulseshape = np.exp(-(ex_vx*(t-ex_offset[3]))**2/ex_sigma[3]**2 - (y - ex_offset[2] - ex_vy*(t-ex_offset[3]))**2/ex_sigma[2]**2)
+        harmonic = 1.0
+    elif ex_type=='simple_pulse2D_x':
+        pulseshape = np.exp(-(ex_vx*(t-ex_offset[3]))**2/ex_sigma[3]**2)
+        harmonic = 1.0
+    elif ex_type=='off':
+        pulseshape = 0.0
+        harmonic = 0.0
+    elif ex_type=='jump':
+        pulseshape = 1.0
+        harmonic = 1.0
+    
     q1  = da.getVecArray(Q1)
     q2  = da.getVecArray(Q2)
     q3  = da.getVecArray(Q3)
 
     if (axis,side) == (0,0) and xi == 0:
-        q2[ 0,:] = 1.0
+        q1[:,:] = 0.0
+        q2[:,:] = zo*ex_amplitude[1]*pulseshape*harmonic
+        q3[:,:] = ex_amplitude[1]*pulseshape*harmonic
     if (axis,side) == (1,0) and yi == 0:
         q1[:, 0] = 0.0
     if (axis,side) == (0,1) and xf == nx-1:
@@ -334,7 +366,7 @@ def aux_bc_pml(pml,pml_type,xi,xf,yi,yf,nx,ny):
     Set  PML on the auxiliary boundary conditions.
     """
     from build_pml import build_pml
-    ddx,ddy,dt,co = 1,1,1,1
+    #    ddx,ddy,dt,co = 1,1,1,1
     build_pml(pml,pml_type,ddx,ddy,dt,norder,Ro,co,xi+1,xf,yi+1,yf,nx,ny)
 
 def aux_bc():
@@ -343,6 +375,25 @@ def aux_bc():
     """
     pass
 
+def write(Q1,Q2,Q3,filename):
+    io = PETSc.Viewer().createBinary(filename,mode="w")
+    Q1.view(io)
+    Q2.view(io)
+    Q3.view(io)
+    io.destroy()
+
+def gauges(da):
+    from probe import Probe
+    entries=[[ 0, 0],
+         [34,10],
+         [34,34],
+         [10,34],
+         [0,15],
+         ]
+    prb = Probe(da, entries)
+    return prb
+
+# -------- MAIN PROGRAM --------------
 
 # create DA and allocate global and local variables
 from petsc4py import PETSc
@@ -378,12 +429,6 @@ s4  = np.zeros([xf-xi,yf-yi], order='F')
 #aux = np.ones ([num_aux,xf-xi,yf-yi], order='F')
 aux = etar(da,ddx,ddy)
 
-#da_pml = PETSc.DA().create([mx,my], dof=num_pml,
-#                       stencil_type=stype,
-#                       stencil_width=swidth)
-#PML = da_pml.createGlobalVec()
-#PMLloc = da_pml.createLocalVec()
-#pml = PMLloc.getArray().reshape([num_pml,gxf-gxi,gyf-gyi], order='F')
 pml = np.ones ([num_pml,xf-xi,yf-yi], order='F')
 pml_axis = 0
 pml_side = 1
@@ -395,22 +440,6 @@ aux_bc_pml(pml,pml_type,xi,xf,yi,yf,nx,ny)
 #    pylab.contourf(pml[i,:,:].copy())
 #    pylab.colorbar()
 #pylab.show()
-
-from probe import Probe
-entries=[[ 0, 0],
-         [34,10],
-         [34,34],
-         [10,34],
-         [0,15],
-         ]
-prb = Probe(da, entries)
-
-def write(Q1,Q2,Q3,filename):
-    io = PETSc.Viewer().createBinary(filename,mode="w")
-    Q1.view(io)
-    Q2.view(io)
-    Q3.view(io)
-    io.destroy()
 
 draw = PETSc.Viewer.DRAW()
 
